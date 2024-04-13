21614c6bfb8a1bcd948b31f1448b7e7988325c24
evgn91 <invaders.of.japan@yandex.ru>
2024-04-13 13:08:05 +0700

    Added prompt for force overwrite existing videos, added prompt for deleting PNGs after conversion is done

--- convert_png_to_video.bat
+++ convert_png_to_video.bat
@@ -4,6 +4,20 @@ setlocal enabledelayedexpansion
 
 chcp 65001
 
+set /P forceOverwrite=Do you wish to force overwrite existing video files (Y/[N])?
+
+if !forceOverwrite! == Y set forceOverwrite=1
+if !forceOverwrite! == y set forceOverwrite=1
+if !forceOverwrite! == N set forceOverwrite=0
+if !forceOverwrite! == n set forceOverwrite=0
+
+set /P removePNG=Do you wish to remove png files after completion (Y/[N])?
+
+if !removePNG! == Y set removePNG=1
+if !removePNG! == y set removePNG=1
+if !removePNG! == N set removePNG=0
+if !removePNG! == n set removePNG=0
+
 set width=1
 set height=1
 set duration=5
@@ -24,6 +38,7 @@ for /r %%F in (*.png) do (
    ) else if !ext! == mov (
       call :mov "%%F"
    )
+   if !removePNG! == 1 del "%%F"
 )
 goto :End
 
@@ -47,23 +62,28 @@ for /F "tokens=1,2 delims=x" %%i in (%size%) do (
 goto:eof
 
 :mpeg2
-ffmpeg -loop 1 -i "%~d1%~p1%~n1%~x1" -t !duration:~0,-1! -pix_fmt yuv420p -vf scale=!width!:!height! -r !fps:~0,-3! -b:v !bitrate:~0,-4!k -c:v mpeg2video -qscale:v 2 "%~d1%~p1%~n1.mpg"
+if !forceOverwrite! == 1 (ffmpeg -y -loop 1 -i "%~d1%~p1%~n1%~x1" -t !duration:~0,-1! -pix_fmt yuv420p -vf scale=!width!:!height! -r !fps:~0,-3! -b:v !bitrate:~0,-4!k -c:v mpeg2video -qscale:v 2 "%~d1%~p1%~n1.mpg") else (
+   ffmpeg -loop 1 -i "%~d1%~p1%~n1%~x1" -t !duration:~0,-1! -pix_fmt yuv420p -vf scale=!width!:!height! -r !fps:~0,-3! -b:v !bitrate:~0,-4!k -c:v mpeg2video -qscale:v 2 "%~d1%~p1%~n1.mpg")
 goto:eof
 
 :mp4
-ffmpeg -loop 1 -i "%~d1%~p1%~n1%~x1" -c:v libx264 -t !duration:~0,-1! -pix_fmt yuv420p -vf scale=!width!:!height! -r !fps:~0,-3! -b:v !bitrate:~0,-4!k -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" "%~d1%~p1%~n1.mp4"
+if !forceOverwrite! == 1 (ffmpeg -y -loop 1 -i "%~d1%~p1%~n1%~x1" -c:v libx264 -t !duration:~0,-1! -pix_fmt yuv420p -vf scale=!width!:!height! -r !fps:~0,-3! -b:v !bitrate:~0,-4!k -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" "%~d1%~p1%~n1.mp4") else (
+   ffmpeg -loop 1 -i "%~d1%~p1%~n1%~x1" -c:v libx264 -t !duration:~0,-1! -pix_fmt yuv420p -vf scale=!width!:!height! -r !fps:~0,-3! -b:v !bitrate:~0,-4!k -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" "%~d1%~p1%~n1.mp4")
 goto:eof
 
 :avi
-ffmpeg -loop 1 -i "%~d1%~p1%~n1%~x1" -c:v libx264 -t !duration:~0,-1! -pix_fmt yuv420p -vf scale=!width!:!height! -r !fps:~0,-3! -b:v !bitrate:~0,-4!k  "%~d1%~p1%~n1.avi"
+if !forceOverwrite! == 1 (ffmpeg -y -loop 1 -i "%~d1%~p1%~n1%~x1" -c:v libx264 -t !duration:~0,-1! -pix_fmt yuv420p -vf scale=!width!:!height! -r !fps:~0,-3! -b:v !bitrate:~0,-4!k  "%~d1%~p1%~n1.avi") else (
+   ffmpeg -loop 1 -i "%~d1%~p1%~n1%~x1" -c:v libx264 -t !duration:~0,-1! -pix_fmt yuv420p -vf scale=!width!:!height! -r !fps:~0,-3! -b:v !bitrate:~0,-4!k  "%~d1%~p1%~n1.avi")
 goto:eof
 
 :xvid
-ffmpeg -loop 1 -i "%~d1%~p1%~n1%~x1" -c:v mpeg4 -vtag xvid -t !duration:~0,-1! -pix_fmt yuv420p -vf scale=!width!:!height! -r !fps:~0,-3! -b:v !bitrate:~0,-4!k  "%~d1%~p1%~n1.avi"
+if !forceOverwrite! == 1 (ffmpeg -y -loop 1 -i "%~d1%~p1%~n1%~x1" -c:v mpeg4 -vtag xvid -t !duration:~0,-1! -pix_fmt yuv420p -vf scale=!width!:!height! -r !fps:~0,-3! -b:v !bitrate:~0,-4!k  "%~d1%~p1%~n1.avi") else (
+   ffmpeg -loop 1 -i "%~d1%~p1%~n1%~x1" -c:v mpeg4 -vtag xvid -t !duration:~0,-1! -pix_fmt yuv420p -vf scale=!width!:!height! -r !fps:~0,-3! -b:v !bitrate:~0,-4!k  "%~d1%~p1%~n1.avi")
 goto:eof
 
 :mov
-ffmpeg -loop 1 -i "%~d1%~p1%~n1%~x1" -c:v libx264 -t !duration:~0,-1! -pix_fmt yuv420p -vf scale=!width!:!height! -r !fps:~0,-3! -b:v !bitrate:~0,-4!k -f mov  "%~d1%~p1%~n1.mov"
+if !forceOverwrite! == 1 (ffmpeg -y -loop 1 -i "%~d1%~p1%~n1%~x1" -c:v libx264 -t !duration:~0,-1! -pix_fmt yuv420p -vf scale=!width!:!height! -r !fps:~0,-3! -b:v !bitrate:~0,-4!k -f mov  "%~d1%~p1%~n1.mov") else (
+   ffmpeg -loop 1 -i "%~d1%~p1%~n1%~x1" -c:v libx264 -t !duration:~0,-1! -pix_fmt yuv420p -vf scale=!width!:!height! -r !fps:~0,-3! -b:v !bitrate:~0,-4!k -f mov  "%~d1%~p1%~n1.mov")
 goto:eof
 
 :End
